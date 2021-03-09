da9388f7383ad58892a6513b00e906dc4ef57e88
luisg17 <33740789+luisg17@users.noreply.github.com>
2021-03-09 15:51:48 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
-//Revisar si el navegador soporta service worker
+//Validar si estamos en local host o en el 
 
+const url = window.location.href;
+const swLocation ='/WebAppCitas/sw.js';
+
+
+//Revisar si el navegador soporta service worker
 if('serviceWorker' in navigator){
+
+    if(url.includes('localhost')){
+        swLocation = '/sw.js';
+    }
+
     //registramos nuestro sw.js y esto nos retornara un Promise
-    navigator.serviceWorker.register('./sw.js')
+    navigator.serviceWorker.register(swLocation)
         .then(registrado => console.log('Se instalo correctamente', registrado))
         .catch(error => console.log('Fallo la instalacion', error))
 }else{
